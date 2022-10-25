c5fcdd53f3e84b2b678064809c7855a396f08da9
sonoda03 <proyou2022@gmail.com>
2022-10-25 16:41:17 +0000

    [FInish]publicのdevise/controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  root 'public/homes#top'
+  get '/about' => 'public/homes#about'
+
+  namespace :public do
+
+  end
   devise_for :admins
-  devise_for :customers
+
+
+
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
