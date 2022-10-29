d11b8fb179117435d084f9a2668645422e505666
sonoda03 <proyou2022@gmail.com>
2022-10-29 00:19:36 +0000

    [Add]商品画像とジャンル選択ボックス追加完了

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,14 +2,13 @@ class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def new
-     @item_new = Item.new
+    @item = Item.new
   end
 
   def create
-     @item_new = Item.new(item_params)
-    if @item_new.save
-      redirect_to admin_item_path(params[:id])
-      flash[:notice] = "商品登録完了しました。"
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
     else
       render 'new'
     end
@@ -28,20 +27,22 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to admin_item_path(params[:id])
-      flash[:notice] = "更新完了しました。"
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:notice] = "商品を更新しました。"
+      redirect_to admin_item_path(@item.id)
     else
       render 'edit'
     end
   end
 
+
+
   private
 
   def item_params
-
-    params.require(:item).permit(:name,:introduction,:unit_price,:is_on_sale)
+    params.require(:item).permit(:name, :introduction, :unit_price, :is_on_sale, :item_image, :genre_id)
   end
+
 
 end
